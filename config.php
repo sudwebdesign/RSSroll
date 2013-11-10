c1a0ed8c7203178948ed05f991a0db8a1cf034d8
Thomas Ingles <thomas@sudwebdesign.fr>
2013-11-10 12:20:43 -0800

    curl or js ?

--- config.php
+++ config.php
@@ -3,8 +3,8 @@
  * Plugin rssroll
  *
  * @package	PLX
- * @version	1.2
- * @date	09/11/2013
+ * @version	1.2.1
+ * @date	10/11/2013
  * @author	i M@N
  * @based on	Rockyhorror Blogroll
  **/
@@ -17,6 +17,7 @@
 	if(!empty($_POST)) {
 		$plxPlugin->setParam('rssroll', $_POST['rssroll'], 'cdata');
 		$plxPlugin->setParam('pub_title', $_POST['pub_title'], 'cdata');
+		$plxPlugin->setParam('curl_or_js', $_POST['curl_or_js'], 'string');
 		$plxPlugin->saveParams();
 		header('Location: parametres_plugin.php?p=RSSroll');
 		exit;
@@ -33,7 +34,9 @@
 		
 		<p><?php echo $plxPlugin->getLang('L_CONFIG_PUB_TITLE') ?></p>
 		<?php plxUtils::printInput('pub_title', $plxPlugin->getParam('pub_title'), 'text'); ?>
-	</fieldset>
+
+		<p><?php echo $plxPlugin->getLang('L_CONFIG_CURL_OR_JS') ?></p>
+		<?php plxUtils::printSelect('curl_or_js',array('1'=>$plxPlugin->getLang('L_YES'),'0'=>$plxPlugin->getLang('L_NO')),$parms['curl_or_js']) ?>	</fieldset>
 	<br />
 	<?php echo plxToken::getTokenPostMethod() ?>
 	<input type="submit" name="submit" value="<?php echo $plxPlugin->getLang('L_CONFIG_SAVE') ?>" />
