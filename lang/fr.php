be8abc2e3d0e2d8bd0857a56dee8e63b8f4fb2ef
Thomas Ingles <thomas@sudwebdesign.fr>
2013-11-10 12:17:44 -0800

    curl or js ?

--- lang/fr.php
+++ lang/fr.php
@@ -1,22 +1,24 @@
 <?php
-	$LANG = array(
-		'L_TITLE'		=>	'RSSroll',
-		'L_CONFIG_DESCRIPTION'	=>	'Page de configuration du plugin RSSroll',
-		'L_CONFIG_ROOT_PATH'	=>	'Emplacement du fichier du blogroll (fichier xml)',
-		'L_CONFIG_SAVE'		=>	'Enregistrer',
-		'L_CONFIG_PUB_TITLE'	=>	'Afficher en tant que (partie publique)',
-		'L_ADMIN_DESCRIPTION'	=>	'Page d\'administration du plugin RSSroll',
-		'L_ADMIN_APPLY_BUTTON'	=>	'Mettre à jour',
-		'L_ADMIN_SELECTION'	=>	'Pour la séléction',
-		'L_ADMIN_DELETE'	=>	'Effacer',
-		'L_ADMIN_LIST_ID'	=>	'Identifiant',
-		'L_ADMIN_LIST_NAME'	=>	'Titre',
-		'L_ADMIN_LIST_URL'	=>	'Feed URL',
-		'L_ADMIN_LIST_DESC'	=>	'Description',
-		'L_ADMIN_LIST_LANG'	=>	'Langue',
-		'L_ADMIN_LIST_LINK'	=>	'Flux',
-		'L_ADMIN_LIST_NEW'	=>	'Nouveau',
-		'L_ADMIN_LIST_ORDER'	=>	'Ordre'
-		
-	);
+$LANG = array(
+'L_TITLE'		=>	'RSSroll',
+'L_CONFIG_DESCRIPTION'	=>	'Page de configuration du plugin RSSroll',
+'L_CONFIG_ROOT_PATH'	=>	'Emplacement du fichier du blogroll (fichier xml)',
+'L_CONFIG_SAVE'		=>	'Enregistrer',
+'L_CONFIG_PUB_TITLE'	=>	'Afficher en tant que (partie publique)',
+'L_CONFIG_CURL_OR_JS'	=>	'Utiliser CURL (si disponible)',
+'L_ADMIN_DESCRIPTION'	=>	'Page d\'administration du plugin RSSroll',
+'L_ADMIN_APPLY_BUTTON'	=>	'Mettre à jour',
+'L_ADMIN_SELECTION'	=>	'Pour la séléction',
+'L_ADMIN_DELETE'	=>	'Effacer',
+'L_ADMIN_LIST_ID'	=>	'Identifiant',
+'L_ADMIN_LIST_NAME'	=>	'Titre',
+'L_ADMIN_LIST_URL'	=>	'Feed URL',
+'L_ADMIN_LIST_DESC'	=>	'Description',
+'L_ADMIN_LIST_LANG'	=>	'Langue',
+'L_ADMIN_LIST_LINK'	=>	'Flux',
+'L_ADMIN_LIST_NEW'	=>	'Nouveau',
+'L_ADMIN_LIST_ORDER'	=>	'Ordre',
+'L_YES'			=> 	'Oui',
+'L_NO'			=> 	'Non'
+);
 ?>
