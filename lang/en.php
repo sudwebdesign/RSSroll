749b961c8f57753834ebcaf24a5df9c60d37ffc2
Thomas Ingles <thomas@sudwebdesign.fr>
2013-12-01 13:14:59 -0800

    Update en.php

--- lang/en.php
+++ lang/en.php
@@ -1,25 +1,28 @@
 <?php
-$LANG = array(
-'L_TITLE'		=>	'RSSroll',
+	$LANG = array(
+'L_TITLE'				=>	'RSSroll',
+'L_POSTED_ON'			=>	'Posted on',
 'L_CONFIG_DESCRIPTION'	=>	'RSSroll configuration page',
 'L_CONFIG_ROOT_PATH'	=>	'RSSroll file location (xml file)',
-'L_CONFIG_SAVE'		=>	'Save',
+'L_CONFIG_SAVE'			=>	'Save',
 'L_CONFIG_PUB_TITLE'	=>	'Display as (Public part)',
-'L_CONFIG_CURL_OR_JS'	=>	'Use CURL (if loaded)',
+'L_CONFIG_TRUNCATE'		=>	'Number of carcters before truncation',
+'L_CONFIG_CURLONLY'		=>	'Use CURL (if loaded)',
 'L_ADMIN_DESCRIPTION'	=>	'RSSroll administration page',
 'L_ADMIN_APPLY_BUTTON'	=>	'Update',
-'L_ADMIN_SELECTION'	=>	'Dor selection',
-'L_ADMIN_DELETE'	=>	'Delete',
-'L_ADMIN_LIST_ID'	=>	'Id',
-'L_ADMIN_LIST_NAME'	=>	'Title',
-'L_ADMIN_LIST_URL'	=>	'Feed URL',
-'L_ADMIN_LIST_DESC'	=>	'Description',
-'L_ADMIN_LIST_LANG'	=>	'Language',
-'L_ADMIN_LIST_LINK'	=>	'Feed',
-'L_ADMIN_LIST_NEW'	=>	'New',
+'L_ADMIN_SELECTION'		=>	'Dor selection',
+'L_ADMIN_DELETE'		=>	'Delete',
+'L_ADMIN_LIST_ID'		=>	'Id',
+'L_ADMIN_LIST_NAME'		=>	'Title',
+'L_ADMIN_LIST_URL'		=>	'Feed URL',
+'L_ADMIN_LIST_DESC'		=>	'Description',
+'L_ADMIN_LIST_LANG'		=>	'Language',
+'L_ADMIN_LIST_LINK'		=>	'Feed',
+'L_ADMIN_LIST_NEW'		=>	'New',
 'L_ADMIN_LIST_ORDER'	=>	'Order',
-'L_YES'			=>	'Yes',
-'L_NO'			=>	'No'
-);
+'L_YES'					=> 'Yes',
+'L_NO'					=> 'No'
+	);
 ?>
+
 
